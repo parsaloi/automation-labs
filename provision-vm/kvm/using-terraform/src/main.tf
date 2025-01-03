1eb20b20583e64187613ea6ffe9e4ad4bd1b553d
Parsaloi <parsaloi35@gmail.com>
2025-01-03 17:32:50 +0300

    automating KVM instance deployment

---
+++ provision-vm/kvm/using-terraform/src/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu:///system"
+  #alias = "server2"
+  #uri   = "qemu+ssh://root@192.168.100.10/system"
+}
