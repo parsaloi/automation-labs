4e17b9ce2c327e71d55840ffc611ff98cc7687bb
Parsaloi <parsaloi35@gmail.com>
2025-01-09 17:37:11 +0300

    some minimal changes

--- provision-vm/kvm/using-terraform/src/storage.tf
+++ provision-vm/kvm/using-terraform/src/storage.tf
@@ -1,20 +1,20 @@
-resource "libvirt_pool" "sysimg" {
-  name = "sysimg"
-  type = "dir"
-  target {
-    path = "/var/lib/libvirt/images/more"
+resource "libvirt_pool" "vmdisks" {
+ name = "vmdisks"
+ type = "dir"
+ target {
+   path = "/vmdisks"
+ }
   }
-}
 
 resource "libvirt_volume" "arch_iso" {
   name   = "archlinux-2024.08.01-x86_64.iso"
-  pool   = libvirt_pool.sysimg.name
+  pool   = libvirt_pool.vmdisks.name
   source = "/var/lib/libvirt/images/archlinux-2024.08.01-x86_64.iso"
 }
 
 resource "libvirt_volume" "node0_disk" {
   name   = "${var.domain_name}.qcow2"
-  pool   = libvirt_pool.sysimg.name
+  pool   = libvirt_pool.vmdisks.name
   size   = var.disk_size * 1024 * 1024 * 1024
   format = "qcow2"
 }
